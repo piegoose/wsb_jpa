4bccefe38aeb5ed98e9aa8f2d0c6a180f90833e3
4lekz <mac.max.aleksander@gmail.com>
2025-01-08 23:24:29 +0100

    lab3
    testy PatientDaoImpl

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/PatientDao.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/PatientDao.java
@@ -1,13 +1,16 @@
 package com.jpacourse.persistence.dao;
 
 import com.jpacourse.persistence.entity.PatientEntity;
+import com.jpacourse.persistence.entity.VisitEntity;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PatientDao extends Dao<PatientEntity, Long> {
 
-//    List<PatientEntity> findByLastName(String lastName);
+    List<PatientEntity> findBySurnameQuery(String lastName);
     void addVisitToPatient(Long patientId, Long doctorId, LocalDateTime visitDate, String description, List<String> treatmentTypes);
-
+    List<VisitEntity> findVisitsByPatientIdQuery(Long patientId);
+    List<PatientEntity> findPatientsWithMoreThanXVisitsQuery(Long numberOfVisits);
+    List<PatientEntity> findPatientsByInsuranceQuery(Boolean hasInsurence);
 }
--- wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/PatientDaoImpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/impl/PatientDaoImpl.java
@@ -19,12 +19,15 @@ import java.util.stream.Collectors;
 
 @Repository
 public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao {
-//    @Override
-//    public List<PatientEntity> findByLastName(String lastName) {
-//        return entityManager.createNativeQuery("SELECT * FROM patient WHERE last_name = ?", PatientEntity.class)
-//                .setParameter(1, lastName)
-//                .getResultList();
-//    }
+
+    @Override
+    public List<PatientEntity> findBySurnameQuery(String lastName) {
+        return entityManager.createQuery(
+                "SELECT p FROM PatientEntity p " +
+                        "WHERE p.lastName = :lastName", PatientEntity.class)
+                .setParameter("lastName", lastName)
+                .getResultList();
+    }
 
     @Override
     @Transactional
@@ -56,6 +59,32 @@ public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements 
         patient.getVisits().add(visitEntity);
 
         update(patient);
+    }
+
+    @Override
+    public List<VisitEntity> findVisitsByPatientIdQuery(Long patientId) {
+        return entityManager.createQuery(
+                "SELECT v from VisitEntity v "+
+                        "where v.patient.id = :patientId", VisitEntity.class).setParameter("patientId", patientId).getResultList();
+    }
+
+    @Override
+    public List<PatientEntity> findPatientsWithMoreThanXVisitsQuery(Long numberOfVisits) {
+        return entityManager.createQuery(
+                "SELECT p FROM PatientEntity p " +
+                        "JOIN p.visits v " +
+                        "GROUP BY p " +
+                        "HAVING COUNT(v) > :xnumber", PatientEntity.class)
+                .setParameter("xnumber", numberOfVisits)
+                .getResultList();
+    }
+
+    @Override
+    public List<PatientEntity> findPatientsByInsuranceQuery(Boolean hasInsurence) {
+        return entityManager.createQuery(
+                "SELECT P FROM PatientEntity P " +
+                        "WHERE P.hasInsurence = :hasInsurence", PatientEntity.class)
+                .setParameter("hasInsurence", hasInsurence).getResultList();
     }
 
 }
